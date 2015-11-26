4477fe613aac77d39e3ed6d5ad4ddcba499ca6a8
MiranBatti <miran.batti@gmail.com>
2015-11-26 13:10:48 +0100

    lab2

---
+++ src/se/hig/aod/lab2/LinkedList.java
@@ -0,0 +1,83 @@
+package se.hig.aod.lab2;
+
+public class LinkedList<T> implements List<T> {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int numberOfElements() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void insertFirst(T t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertLast(T t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void printList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printListR() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reversePrintList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
