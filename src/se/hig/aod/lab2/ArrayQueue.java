4e9e6d5ce68307f619c8b44cad5171cfbed6ac5e
MiranBatti <miran.batti@gmail.com>
2015-12-01 14:05:19 +0100

    ArrayQueue

---
+++ src/se/hig/aod/lab2/ArrayQueue.java
@@ -0,0 +1,35 @@
+package se.hig.aod.lab2;
+
+public class ArrayQueue<V> implements Queue<V>{
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void enqueue(V v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public V dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V getFront() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
