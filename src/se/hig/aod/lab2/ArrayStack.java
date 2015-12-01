e5828f8e81835cd1b5dbae79fd5212f696b706e4
Frekkan <fredrik.lindorf@gmail.com>
2015-12-01 13:37:57 +0100

    ArrayStack

---
+++ src/se/hig/aod/lab2/ArrayStack.java
@@ -0,0 +1,36 @@
+package se.hig.aod.lab2;
+
+public class ArrayStack implements Stack {
+	
+	int arrayS[] = new int[10];
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void push(Object v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
