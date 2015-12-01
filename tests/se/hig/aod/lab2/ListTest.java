a7a8a9e7ac2676c814b46546b5aed737d22aeeb1
MiranBatti <miran.batti@gmail.com>
2015-12-01 16:48:34 +0100

    JUnit test

--- tests/se/hig/aod/lab2/ListTest.java
+++ tests/se/hig/aod/lab2/ListTest.java
@@ -64,20 +64,39 @@ public class ListTest {
 	 * Sätt in ett element i listan och sedan en till som kommer vara först i listan.
 	 */
 	@Test
-	public void insertFirstAndReplaceTest() {
+	public void insertFirstWhenListIsPopulated() {
 		testList.insertFirst(2);
 		testList.insertFirst(1);
 		assertEquals(1, testList.getFirst().intValue());
 	}
+	/**
+	 * Testar om värdet i första elementet i listan stämmer och om listan inte är tom efter
+	 * ett element läggs till först i listan.
+	 */
 	@Test
-	public void insertObjectWhenEmpty() {
-		if (testList.isEmpty()) {
-			try { 
-				testList.insertFirst(4);
-				
-			} catch (Exception e) {
-				
-			}
-		}
+	public void insertFirstWhenEmpty() {
+		testList.insertFirst(4);
+		assertEquals(4, testList.getFirst().intValue());
+		assertTrue(!testList.isEmpty());
 	}
-}
+	/**
+	 * Testar lägga till element i slutet av listan efter något sätt in slutet av listan.
+	 */
+	@Test
+	public void insertLastWhenListIsPopulated() {
+		testList.insertLast(2);
+		testList.insertLast(1);
+		assertEquals(1, testList.getLast().intValue());
+	}
+	/**
+	 * Testar om värdet i första elementet i listan stämmer och om listan inte är tom efter
+	 * ett element läggs till först i listan.
+	 */
+	@Test
+	public void insertLastWhenEmpty() {
+		testList.insertLast(4);
+		assertEquals(4, testList.getFirst().intValue());
+		assertEquals(4, testList.getLast().intValue());
+		assertTrue(!testList.isEmpty());
+	}
+} // end class
